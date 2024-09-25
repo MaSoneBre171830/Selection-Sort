b0e97cae7066a350afdfb344bdf509860c068a1f
Nenad <76172679+MaSoneBre171830@users.noreply.github.com>
2024-09-25 10:32:27 +0200

    Add files via upload

---
+++ SelectionSort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selection_Sort
+{
+    internal class SelectionSort
+    {
+        public static int[] Sort(int[] arr)
+        {
+            int CurrentMin;
+            int index = 0;
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                CurrentMin = arr[i];
+                for(int j = i + 1; j < arr.Length; j++)
+                {
+                    if (CurrentMin > arr[j])
+                    {
+                        index = j;
+                        CurrentMin = arr[j];
+                    }
+
+                }
+                
+                if(CurrentMin != arr[i])
+                {
+                    int temp = arr[i];
+                    arr[i] = CurrentMin;
+                    arr[index] = temp;
+
+                }
+            }
+
+            return arr;
+        }
+
+        
+    }
+}
